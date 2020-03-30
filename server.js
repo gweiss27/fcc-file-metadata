946c451b1094060c7cbb3d17a5f6351c6e0babe0
Gregory Weiss <gweiss27@gmail.com>
2020-03-30 12:15:05 -0700

    project solution

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/ ' });
 // require and use "multer"...
 
 var app = express();
@@ -10,14 +11,23 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/hello', function(req, res) {
+    res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+    console.log(req.file);
+    res.json({
+        filename: req.file.filename,
+        size: req.file.size
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log(`Node.js listening on port ${PORT}...`);
 });
